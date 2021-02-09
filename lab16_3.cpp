e2ea779711efed820ad1e5d794324cd089871588
thanakrit555 <59599161+thanakrit555@users.noreply.github.com>
2021-02-09 16:40:03 +0700

    Update lab16_3.cpp

--- lab16_3.cpp
+++ lab16_3.cpp
@@ -22,3 +22,23 @@ int main(){
 }
 
 //Write Definition of randomVector() showVector() and dotProduct() here
+vector<int> randomVector(unsigned int N){	
+    vector<int> v;
+    for(unsigned int i = 0; i < N; i++) v.push_back(rand()%10);
+    return v;
+}
+
+void showVector(vector<int> x){
+	cout << "[";
+	for(unsigned int i = 0; i < x.size() ; i++){
+		cout << x[i];
+		if(i==x.size()) cout << "]";
+		else cout << " ";
+	}
+}
+
+int dotProduct(vector<int> x,vector<int> y){
+	int sum = 0;
+	for(unsigned int i = 0; i < x.size(); i++) sum += x[i]*y[i];	
+	return sum;
+}
